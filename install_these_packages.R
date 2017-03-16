4cd3be69bd12b36f5445a88eb89b3ffe29012010
aurielfournier <aurielfournier@gmail.com>
2017-03-16 12:04:48 -0500

    packages to install

---
+++ install_these_packages.R
@@ -0,0 +1,13 @@
+# install these packages
+
+
+install.packages(c("gapminder","tidyverse","ggthemes","RColorBrewer","gridExtra","cowplot","roxygen2","devtools"))
+
+library(gapminder)
+library(tidyverse)
+library(ggthemes)
+library(RColorBrewer)
+library(gridExtra)
+library(cowplot)
+library(roxygen2)
+library(devtools)
